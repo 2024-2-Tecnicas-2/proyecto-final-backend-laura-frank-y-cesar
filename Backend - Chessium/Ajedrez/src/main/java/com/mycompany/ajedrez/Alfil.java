6a8c283ceeb443b8abd6a127b9a6bc3d79019d38
limon <kilo@gmail.com>
2024-11-13 18:58:20 -0500

    Alfil
    Se corrigen movimientos de alfil

--- Backend - Chessium/Ajedrez/src/main/java/com/mycompany/ajedrez/Alfil.java
+++ Backend - Chessium/Ajedrez/src/main/java/com/mycompany/ajedrez/Alfil.java
@@ -1,5 +1,6 @@
-
 package com.mycompany.ajedrez;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -16,33 +17,32 @@ public class Alfil extends Pieza{
         int[] posicionActual = getPosicion();
         int x = posicionActual[0];
         int y = posicionActual[1];
-        int[][] movimientos = new int[28][2];
-        int con = 0;
+        List<int[]> movimientos = new ArrayList<>();
         
-        for (int i = 1; i < 8; i++) {
-        movimientos[con][0] = x + i;
-        movimientos[con][1] = y + i;
-        con++;
+        int l = 1;
+        while (x + l <= 7 && y + l <= 7) {
+            movimientos.add(new int[]{x + l, y + l});
+            l++;
         }
 
-        for (int i = 1; i < 8; i++) {
-        movimientos[con][0] = x + i;
-        movimientos[con][1] = y - i;
-        con++;
+        int k = 1;
+            while (x + k <= 7 && y - k >= 0) {
+            movimientos.add(new int[]{x + k, y - k});
+            k++;
         }
 
-        for (int i = 1; i < 8; i++) {
-        movimientos[con][0] = x - i;
-        movimientos[con][1] = y + i;
-        con++;
-        }
-
-        for (int i = 1; i < 8; i++) {
-        movimientos[con][0] = x - i;
-        movimientos[con][1] = y - i;
-        con++;
+        int h = 1;
+            while (h < 8 && x - h >= 0 && y + h <= 7)  {
+            movimientos.add(new int[]{x - h, y + h});
+            h++;
         }
         
-        return movimientos;
+        int j = 1;
+        while (x - j >= 0 && y - j >= 0) {
+            movimientos.add(new int[]{x - j, y - j});
+            j++;
+        }
+        
+        return movimientos.toArray(new int[0][0]);
     } 
 }
