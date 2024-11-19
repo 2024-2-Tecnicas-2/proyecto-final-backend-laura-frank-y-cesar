ff1e50e1e76c4f46d8f3798385abfd38148420f1
Autor <Autor@konradlorenz.edu.co>
2024-11-18 19:50:23 -0500

    movimientos del rey

--- Backend - Chessium/Ajedrez/src/main/java/com/mycompany/ajedrez/Partida.java
+++ Backend - Chessium/Ajedrez/src/main/java/com/mycompany/ajedrez/Partida.java
@@ -315,4 +315,38 @@ public class Partida {
 
         return movimientos;
     }
+   
+
+    public ArrayList<int[]> validarMovimientosRey(Pieza pieza) {
+        int[] posicionActual = pieza.getPosicion();
+        int x = posicionActual[0];
+        int y = posicionActual[1];
+        ArrayList<int[]> movimientos = new ArrayList<>();
+
+        
+        int[][] posiblesMovimientos = {
+            {-1, -1}, {-1, 0}, {-1, 1}, 
+            {0, -1}, {0, 1}, 
+            {1, -1}, {1, 0}, {1, 1} 
+        };
+
+       
+        for (int[] movimiento : posiblesMovimientos) {
+            int nuevaX = x + movimiento[0];
+            int nuevaY = y + movimiento[1];
+
+            
+            if (nuevaX >= 0 && nuevaX < 8 && nuevaY >= 0 && nuevaY < 8) {
+                
+                if (!tablero.casillas[nuevaX][nuevaY].tienePieza()
+                        || !tablero.casillas[nuevaX][nuevaY].getPieza().getPropietario().equals(pieza.getPropietario())) {
+                    movimientos.add(new int[]{nuevaX, nuevaY});
+                }
+            }
+        }
+
+        return movimientos;
+    }
+
+   
 }
