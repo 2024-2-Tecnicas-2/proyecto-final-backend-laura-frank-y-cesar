f44f9a6dd2f43569c77e4e48a1b644c420596c02
Name <correo@gmai.com>
2024-11-18 19:52:42 -0500

    Se crea la clase controladora para manejar la partida

---
+++ Backend - Chessium/Ajedrez/src/main/java/com/mycompany/ajedrez/ControladorDePartida.java
@@ -0,0 +1,17 @@
+package com.mycompany.ajedrez;
+
+public class ControladorDePartida {
+    private Partida partida;
+    private Jugador jugBlancas;
+    private Jugador jugNegras;
+    private Jugador jugActual;
+    private boolean juegoTerminado;
+    
+    public ControladorDePartida() {
+        this.partida = new Partida();
+        this.jugBlancas = partida.getJugBlancas();
+        this.jugNegras = partida.getJugNegras();
+        this.jugActual = jugBlancas;
+        this.juegoTerminado = false;
+    }
+}
