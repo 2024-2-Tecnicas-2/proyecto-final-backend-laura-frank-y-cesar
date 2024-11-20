f377e33221aa1d07efb304135b7e2bb417ffa2f5
Autor <Autor@konradlorenz.edu.co>
2024-11-20 18:42:09 -0500

    peon

--- Backend - Chessium/Ajedrez/src/main/java/com/mycompany/ajedrez/Peon.java
+++ Backend - Chessium/Ajedrez/src/main/java/com/mycompany/ajedrez/Peon.java
@@ -28,35 +28,62 @@ public class Peon extends Pieza{
         int y = posicionActual[1];
         ArrayList<int[]> movimientos = new ArrayList<>();
 
-        int direccion = pieza.getPropietario().esBlancas() ? 1 : -1;
+        if (pieza.getPropietario().esBlancas()) {
 
-        if (x + direccion >= 0 && x + direccion < 8) {
-            if (!Tablero.casillas[x + direccion][y].tienePieza()) {
-                movimientos.add(new int[]{x + direccion, y});
+            if (x + 1 < 8) {
+                if (!Tablero.casillas[x + 1][y].tienePieza()) {
+                    movimientos.add(new int[]{x + 1, y});
+                }
             }
-        }
 
-        if ((pieza.getPropietario().esBlancas() && x == 1) || !(pieza.getPropietario().esBlancas() && x == 6)) {
-            if (!Tablero.casillas[x + direccion][y].tienePieza() && !Tablero.casillas[x + 2 * direccion][y].tienePieza()) {
-                movimientos.add(new int[]{x + 2 * direccion, y});
+            if (x == 1) {
+                if (!Tablero.casillas[x + 1][y].tienePieza() && !Tablero.casillas[x + 2][y].tienePieza()) {
+                    movimientos.add(new int[]{x + 2, y});
+                }
             }
-        }
 
-        if (y - 1 >= 0 && x + direccion >= 0 && x + direccion < 8) {
-            if (Tablero.casillas[x + direccion][y - 1].tienePieza()
-                    && !Tablero.casillas[x + direccion][y - 1].getPieza().getPropietario().equals(pieza.getPropietario())) {
-                movimientos.add(new int[]{x + direccion, y - 1});
+            if (y - 1 >= 0 && x + 1 < 8) {
+                if (Tablero.casillas[x + 1][y - 1].tienePieza()
+                        && !Tablero.casillas[x + 1][y - 1].getPieza().getPropietario().equals(pieza.getPropietario())) {
+                    movimientos.add(new int[]{x + 1, y - 1});
+                }
             }
-        }
 
-        if (y + 1 < 8 && x + direccion >= 0 && x + direccion < 8) {
-            if (Tablero.casillas[x + direccion][y + 1].tienePieza()
-                    && !Tablero.casillas[x + direccion][y + 1].getPieza().getPropietario().equals(pieza.getPropietario())) {
-                movimientos.add(new int[]{x + direccion, y + 1});
+            if (y + 1 < 8 && x + 1 < 8) {
+                if (Tablero.casillas[x + 1][y + 1].tienePieza()
+                        && !Tablero.casillas[x + 1][y + 1].getPieza().getPropietario().equals(pieza.getPropietario())) {
+                    movimientos.add(new int[]{x + 1, y + 1});
+                }
+            }
+        } else if (!pieza.getPropietario().esBlancas()) {
+
+            if (x - 1 >= 0) {
+                if (!Tablero.casillas[x - 1][y].tienePieza()) {
+                    movimientos.add(new int[]{x - 1, y});
+                }
+            }
+
+            if (x == 6) {
+                if (!Tablero.casillas[x - 1][y].tienePieza() && !Tablero.casillas[x - 2][y].tienePieza()) {
+                    movimientos.add(new int[]{x - 2, y});
+                }
+            }
+
+            if (y - 1 >= 0 && x - 1 >= 0) {
+                if (Tablero.casillas[x - 1][y - 1].tienePieza()
+                        && !Tablero.casillas[x - 1][y - 1].getPieza().getPropietario().equals(pieza.getPropietario())) {
+                    movimientos.add(new int[]{x - 1, y - 1});
+                }
+            }
+
+            if (y + 1 < 8 && x - 1 >= 0) {
+                if (Tablero.casillas[x - 1][y + 1].tienePieza()
+                        && !Tablero.casillas[x - 1][y + 1].getPieza().getPropietario().equals(pieza.getPropietario())) {
+                    movimientos.add(new int[]{x - 1, y + 1});
+                }
             }
         }
 
         return movimientos;
     }
-
 }
